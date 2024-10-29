20a3cca4783cdb2aa93d7da8b8fc6f91749a7aed
unknown <you@example.com>
2024-10-29 23:55:46 +0800

    updated code in index.php

--- index.php
+++ index.php
@@ -100,7 +100,4 @@ require_once(ROOT_DIR."includes/header.php");
     <!-- Footer -->
     <?php require_once(ROOT_DIR."includes/footer.php"); ?>
 
-    <!-- Bootstrap 5 JS Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+ 
